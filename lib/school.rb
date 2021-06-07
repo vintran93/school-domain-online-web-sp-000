f007cda0cc23e43169ba07233576d4cbf73c0575
Vincent Tran <vincentuong@yahoo.com>
2021-06-07 04:15:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,8 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster.has_key?(grade)
-      @roster[grade] << name
+    if roster.has_key?(grade)
+      roster[grade] << name
     else
       @roster[grade] = [name]
     end 
